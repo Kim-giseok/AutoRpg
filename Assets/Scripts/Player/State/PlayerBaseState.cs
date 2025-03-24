f84ff3fb58a4b4a732c922893ca0212f581e006c
Kim-giseok <belbeck@naver.com>
2025-03-24 14:02:24 +0900

    [Feat] 상태 머신 준비

---
+++ Assets/Scripts/Player/State/PlayerBaseState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBaseState : IState
+{
+    public virtual void Enter()
+    {
+    }
+
+    public virtual void Exit()
+    {
+    }
+
+    public virtual void PhysicsUpdate()
+    {
+    }
+
+    public virtual void Update()
+    {
+        
+    }
+}
